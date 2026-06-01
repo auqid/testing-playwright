55fe8db8ed4b2a19136e98775e8f4df3325671d4
auqid <auqidirfan100@gmail.com>
2026-06-01 15:09:38 +0530

    created tests to check login

--- tests/homepage.spec.js
+++ tests/homepage.spec.js
@@ -1,8 +1,7 @@
-import { test, expect } from "@playwright/test";
+// import { test, expect } from "@playwright/test";
 
-test("homepage loads successfully", async ({ page }) => {
-  await page.goto("https://automationexercise.com");
-  await expect(page).toHaveTitle(/Automation Exercise/);
-  await page.screenshot({ path: "homepage.png", fullPage: true });
-  await expect(page).toHaveURL("https://automationexercise.com/");
-});
+// test("Homepage should have correct title", async ({ page }) => {
+//   await page.goto("https://example.com");
+//   await expect(page).toHaveURL("https://example.com");
+//   await expect(page).toHaveTitle("Example Domain");
+// });
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,7 +1,9 @@
 import { test, expect } from "@playwright/test";
 
 test("navigate to login page", async ({ page }) => {
-  await page.goto("http://automationexercise.com");
-  await page.getByRole("link", { name: " Signup / Login" }).click();
-  await expect(page).toHaveURL(/login/);
+  await page.goto("https://lawtechai.xyz");
+  const loginlink = page.getByRole("link", { name: "Sign in" });
+  await loginlink.click();
+  await expect(page).toHaveURL("https://lawtechai.xyz/login");
+  await expect(page.getByText("Welcome back")).toBeVisible();
 });
