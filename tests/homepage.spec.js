5c9ff6d3760bbbbcc091830aff9a6460f834fbeb
auqid <auqidirfan100@gmail.com>
2026-06-26 16:52:35 +0530

    changes to folder structure

--- tests/homepage.spec.js
+++
@@ -1,7 +0,0 @@
-// import { test, expect } from "@playwright/test";
-
-// test("Homepage should have correct title", async ({ page }) => {
-//   await page.goto("https://example.com");
-//   await expect(page).toHaveURL("https://example.com");
-//   await expect(page).toHaveTitle("Example Domain");
-// });
