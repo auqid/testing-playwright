582cbaee70ed1aa8c3754dce0d3b02807c81a6d6
auqid <auqidirfan100@gmail.com>
2026-06-26 23:05:40 +0530

    added a test for invalid credentials

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -10,3 +10,13 @@ test("Verify Login feature", async ({ page }) => {
   await expect(page).toHaveURL("https://lawtechai.xyz/app/schedule");
   await expect(page).toHaveTitle(/LawTech AI/);
 });
+
+test("Verfiy the login feature with invalid credentials", async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.navigate();
+  await loginPage.fillLoginForm("admin@lawtech.aiiii", "X9@kP2!m");
+  await loginPage.clickLogin();
+
+  await expect(page.getByText("Wrong email or password")).toBeVisible();
+  await expect(page).not.toHaveURL(/app\/schedule/);
+});
